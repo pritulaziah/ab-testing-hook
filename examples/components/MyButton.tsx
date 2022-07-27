0a405785a14ca43742d6df6c5bfc470121b79a42
pritulaziah <ziahherzen@gmail.com>
2022-07-28 00:26:18 +0500

    pretty example

--- examples/components/MyButton.tsx
+++ examples/components/MyButton.tsx
@@ -1,21 +1,29 @@
 import clsx from "clsx";
 import { useExperimentVariant } from "ab-testing-hook";
 
-const defaultClasses = ['text-white', 'bg-blue-700'];
+const defaultClasses = ["bg-blue-700", "hover:bg-blue-800"];
 const variantClasses = {
-  'variantA': ['text-gray-900', 'bg-white', 'border', 'border-gray-200'],
-  'variantB': ['text-white', 'bg-gray-800'],
+  variantA: ["bg-red-700", "hover:bg-red-800"],
+  variantB: ["bg-purple-700", "hover:bg-purple-800"],
 };
 
 const MyButton = () => {
-  const variantName = useExperimentVariant('new_button');
-  const currentClasses = variantName ? variantClasses[variantName as 'variantA' | 'variantB'] : defaultClasses;
+  const variantName = useExperimentVariant("new_button");
+  const currentClasses = variantName
+    ? variantClasses[variantName as "variantA" | "variantB"]
+    : defaultClasses;
 
   return (
-    <button type="button" className={clsx('font-medium rounded-lg text-sm px-4 py-2', currentClasses)}>
-      {`This is ${variantName || 'default'} button`}
+    <button
+      type="button"
+      className={clsx(
+        "text-white font-medium rounded-lg text-sm px-4 py-2 transition-colors",
+        currentClasses
+      )}
+    >
+      {`This is ${variantName || "default"} button`}
     </button>
-  )
-}
+  );
+};
 
 export default MyButton;
